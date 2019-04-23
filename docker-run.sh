5427b5c9bf9ef288701e2bf965ae7244baa86a81
Bora Ozarslan <borako.ozarslan@gmail.com>
2019-04-23 12:34:27 -0500

    Make docker-run aware of Run and Entrypoint

--- docker-run.sh
+++ docker-run.sh
@@ -38,6 +38,30 @@ fi
 declare -a dockerRun
 index=0
 
+# entrypoint is the initial command if it exists
+entrypoint=$(cat ${configJson} | jq '.config.Entrypoint[]' 2>/dev/null)
+if [[ $? == 0 ]]; then
+	# Command did not fail so entrypoint exists as array
+	while read line; do
+		entrypoint2=$(echo ${line} | sed 's/^"//g')
+		entrypoint3=$(echo ${entrypoint2} | sed 's/"$//g')
+		dockerRun[${index}]=${entrypoint3}
+		index=$((index+1))
+	done < <(cat ${configJson} | jq '.config.Entrypoint[]')
+else
+	# Entrypoint is sometimes not in an array
+	entrypoint=$(cat ${configJson} | jq '.config.Entrypoint')
+	# This will always return 0 because config exists
+	if [[ ${entrypoint} != "null" ]]; then
+		# strip the quotation marks
+		entrypoint2=$(echo ${entrypoint} | sed 's/^"//g')
+		entrypoint3=$(echo ${entrypoint2} | sed 's/"$//g')
+		dockerRun[${index}]=${entrypoint3}
+		index=$((index+1))
+	fi
+fi
+
+# Command is the default parameter/command
 while read line; do
 	arg2=$(echo ${line} | sed 's/^"//g')
 	arg3=$(echo ${arg2} | sed 's/"$//g')
@@ -49,6 +73,31 @@ info "Expected Environment Variables:"
 while read line; do
 	echo -e "\t${line}"
 done < <(cat ${configJson} | jq '.config.Env[]')
+echo ""
+
+# Print out commands docker would have run before running the command
+setup=$(cat ${configJson} | jq '.config.Run[]' 2>/dev/null)
+# Run can be in an array even though most of the time it is not but check for
+# it anyways
+if [[ $? == 0 ]]; then
+	info "Docker's setup commands before running:"
+	while read line; do
+		setup2=$(echo ${line} | sed 's/^"//g')
+		setup3=$(echo ${setup2} | sed 's/"$//g')
+		echo -e "\t${setup3}"
+	done < <(cat ${configJson} | jq '.config.Run[]' 2>/dev/null)
+	echo ""
+else
+	setup=$(cat ${configJson} | jq '.config.Run')
+	# This will always return 0 because config exists
+	if [[ ${setup} != "null" ]]; then
+		info "Docker's setup command before running:"
+		setup2=$(echo ${setup} | sed 's/^"//g')
+		setup3=$(echo ${setup2} | sed 's/"$//g')
+		echo -e "\t${setup3}"
+		echo ""
+	fi
+fi
 
 # Find where the binary is from the assumed PATH
 
